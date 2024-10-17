e5c572c04a6e6f1ccf16ad17a8c3a967a86610ff
stringercube <140909265+stringercube@users.noreply.github.com>
2024-10-17 23:24:42 +0200

    functions added for toggle read and remove books

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.info = function() {
-        let isread = read === "yes" ? "have read." : "have not read.";
+        let isread = this.read === "yes" ? "have read." : "have not read.";
         console.log(this.title + " written by " + this.author
                 + " with " + this.pages + " pages that I " + isread)
     };
@@ -14,8 +14,12 @@ function Book(title, author, pages, read) {
 
 
 function addBookToLibrary() {
-  // do stuff here
+  // append book structure in html with consistent classes
+  // receives input from dialog form
+  // fill in info in html
 }
+
+// populate library with some books
 
 const book1 = new Book('The Catcher in the Rye', 'J.D. Salinger', 214, 'no');
 
@@ -29,4 +33,62 @@ const book3 = new Book('1984', 'George Orwell', 328, 'yes');
 
 myLibrary.push(book3);
 
-console.table(myLibrary);
+// add existing books to a book card in html
+
+document.addEventListener('DOMContentLoaded', () => {
+    const booklist = document.querySelectorAll('.book');
+    let i = -1;
+    booklist.forEach((book) => {
+        i += 1;
+        let title = book.querySelector('.title');
+        let author = book.querySelector('.author');
+        let pages = book.querySelector('.pages');
+        let read = book.querySelector('.read');
+        title.textContent = myLibrary[i].title;
+        author.textContent = myLibrary[i].author;
+        pages.textContent = myLibrary[i].pages;
+        read.textContent = myLibrary[i].read;
+    });    
+});
+
+// function for removing book in each card
+
+function removeBook(event) {
+    const activeBook = event.target.parentNode;
+    activeBook.remove();
+};
+
+// function to toggle read in each card
+
+function toggle(event) {
+    const activeBook = event.target.parentNode;
+    const isread = activeBook.querySelector('.read');
+    
+    if (isread.textContent === 'yes') {
+        isread.textContent = 'no';
+    } else {
+        isread.textContent = 'yes';
+    };
+};
+
+// TO DO add event listeners
+
+// using event delegation
+
+document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('read')) {
+        toggle(event);
+    }
+    if (event.target.classList.contains('remove')) {
+        removeBook(event);
+    }
+});
+
+// dialog opening
+
+const dialog = document.querySelector('dialog');
+const addBook = document.querySelector('.new-book');
+
+addBook.addEventListener('click', () => {
+    dialog.showModal();
+});
